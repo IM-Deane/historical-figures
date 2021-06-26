d3520bfead430087d6df651d11ba57e6edd712a7
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2021-06-26 12:45:01 -0600

    Updated content

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -9,13 +9,13 @@ import {
 	deletePost,
 	likePost,
 } from "../controllers/posts.js";
-
 // Verify the user can access the data before allowing them to complete an action
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/search", getPostsBySearch);
+
 router.get("/:id", getPost);
 router.get("/", getPosts);
 router.post("/", auth, createPost);
